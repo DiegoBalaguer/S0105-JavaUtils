0757d05e7aecfbcef9394edc9fca2bd8aa5b858f
Diego.Balaguer <didacb@gmail.com>
2025-05-02 01:28:31 +0200

    feat(level03): add exercise 01 - file encryption and decryption

---
+++ src/main/java/level03/exercise01/application/ApplicationController.java
@@ -0,0 +1,35 @@
+package level03.exercise01.application;
+
+
+import level03.exercise01.utils.FileEncryptorUtils;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * PROGRAM: ApplicationController
+ * AUTHOR: Diego Balaguer
+ * DATE: 25/04/2025
+ */
+
+public class ApplicationController {
+
+    public void run() {
+        System.out.println("* STARTING PROCESS..........." + System.lineSeparator());
+
+        Path inputFile = Path.of("src/main/resources/directoryEstructure.txt");
+        Path encryptedFile = Path.of("src/main/resources/directoryEstructure.encrypted");
+        Path decryptedFile = Path.of("src/main/resources/directoryEstructure.decrypted");
+        System.out.println("** Load y encrypt ..........." + System.lineSeparator());
+        try {
+            SecretKey key = FileEncryptorUtils.generateSecretKey();
+            FileEncryptorUtils.encryptFile(inputFile, encryptedFile, key);
+
+            FileEncryptorUtils.decryptFile(encryptedFile, decryptedFile, key);
+            System.out.println("✅ process completed correctly");
+        } catch (Exception e) {
+            System.err.println("❌ Error: " + e.getMessage());
+        }
+    }
+}
